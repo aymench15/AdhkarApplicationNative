acb6701a48c2af7755eba80c60129a97fed3e16e
mad-7 <95884764+mad-7@users.noreply.github.com>
2022-05-04 12:35:14 +0200

    Adkkar sahab & masaa Functions

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,30 +3,44 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.example.myapplication.Adapter.AdhkarListAdapter;
+import com.example.myapplication.DataBase.TestDataBase;
+import com.example.myapplication.ui.AdhkarSabahActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class MainActivity extends AppCompatActivity {
-private Button btnClick;
+private ImageButton adhkarSabah;
+private ImageButton adhkarMasaa;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // btnClick = (Button) findViewById(R.id.button) ;
-      //  TextView tv2 = (TextView) findViewById(R.id.textView);
-      //  tv2.setText("here is home");
 
-       // ImageView img=
         BottomNavigationView btmNav = findViewById(R.id.btmNav);
         btmNav.setSelectedItemId(R.id.home);
         btmNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -50,30 +64,23 @@ private Button btnClick;
 
             }
         });
-/*
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.notification_icon)
-                .setContentTitle(textTitle)
-                .setContentText(textContent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);*/
+
+        adhkarSabah = findViewById(R.id.sabah_adhkar);
+        adhkarSabah.setOnClickListener(view-> {
+            Intent intent = new Intent(this, AdhkarSabahActivity.class);
+            intent.putExtra("filename","sabah.txt");
+            startActivity(intent);
+        });
+        adhkarMasaa = findViewById(R.id.masaa_adhkar);
+        adhkarMasaa.setOnClickListener(view-> {
+            Intent intent = new Intent(this, AdhkarSabahActivity.class);
+            intent.putExtra("filename","masaa.txt");
+            startActivity(intent);
+        });
 
 
 
-   /* public void onClick(View v) {
 
-           // EditText tv=(EditText) findViewById(R.id.tv1);
-            Intent myIntent=new Intent(this,MainActivity2.class);
-            Bundle b=new Bundle();
-            b.putString("text",tv.getText().toString());
-            myIntent.putExtras(b);
-            startActivity(myIntent);
-          //  Toast.makeText(this,"aimn",Toast.LENGTH_LONG).show();
-
-
-        }*/
-        //  .setOnClickListener(new View.OnClickListener() {
-
-        //  });
     }
     public void gone(View view){
         FrameLayout fl =(FrameLayout) findViewById(R.id.frameLayout3);
@@ -83,5 +90,7 @@ private Button btnClick;
         FrameLayout fl =(FrameLayout) findViewById(R.id.frameLayout3);
         fl.setVisibility(view.INVISIBLE);
     }
+
+
 
     }
